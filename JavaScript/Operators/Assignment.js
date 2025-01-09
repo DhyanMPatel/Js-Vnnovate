4ae43db44b40229672237b6386935032efa1a933
jonathan <jonathan@growth2success.io>
2025-01-09 18:11:50 +0530

    Temp complete Assignment Operator

--- JavaScript/Operators/Assignment.js
+++ JavaScript/Operators/Assignment.js
@@ -37,8 +37,8 @@ let bigint = 5n
 
 /// /=
 // console.log(c/=2); // Return - 5
-console.log(str /= 5); // Return - NaN
-console.log(bool /= 5); // Return - o.2
+// console.log(str /= 5); // Return - NaN
+// console.log(bool /= 5); // Return - o.2
 // console.log(bigint *= 5); // Error
 
 /// %=
@@ -62,10 +62,64 @@ console.log(bool /= 5); // Return - o.2
 /// ^=
 
 
-/// &&=
+/// &&=  // Means: x && (x = y)
+//      - if first operand will be truthy then second operand value will assigned to first operand
+
+let name = { 
+    firstName: "Ram", 
+    lastName: "", 
+  }; 
+    
+  console.log(name.firstName); 
+    
+  // Changing the value using logical 
+  // AND assignment operator 
+  name.firstName &&= "Shyam"; 
+    
+  // Here the value changed because 
+  // name.firstName is truthy 
+  console.log(name.firstName); 
+    
+  console.log(name.lastName); 
+    
+  // Changing the value using logical  
+  // AND assignment operator 
+  name.lastName &&= "Kumar"; 
+    
+  // Here the value remains unchanged  
+  // because name.lastName is falsy 
+  console.log(name.lastName); 
 
 
-/// ||=
 
 
-/// ??=
+/// ||=  // Means: x || (x=y)
+//      - if first operand will be falsy then second operand value will assigned to first operand
+
+  console.log(name.firstName); 
+  // Changing the value using logical 
+  // OR assignment operator 
+  name.firstName ||= "Shyam"; 
+  // But value does not change because 
+  // name.firstName is truthy 
+  console.log(name.firstName);  
+  console.log(name.lastName); 
+  // Changing the value using logical 
+  // OR assignment operator 
+  name.lastName ||= "Kumar"; 
+  // The value changes because name.lastName is falsy 
+  console.log(name.lastName); 
+
+
+
+/// ??=  // Means: x ?? (x = y)
+//      - In Object there is a properties is not defined which is indicate as a undefined so ??= will create that undefined property and assigned right side value
+let one = 1
+let empty1 = null
+let empty2 = undefined
+let three = 3
+console.log(one ??= empty1); // Return - 1
+console.log(empty2 ??= empty1); // Return - null    last value
+console.log(empty1 ??= three); // Return - 3
+
+
