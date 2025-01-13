7d5565dd9d290ffdde7f6c91594b087d0ca90332
jonathan <jonathan@growth2success.io>
2025-01-13 15:42:39 +0530

    map conplete

--- JavaScript/04_Data_Types/Iterable.js
+++ JavaScript/04_Data_Types/Iterable.js
@@ -2,10 +2,33 @@
 //  - Ex - Array, String,
 
 // range Object
-let range = {
+let range1 = {
   from: 2,
   to: 6,
 };
-for (let num of range) {
+for (let num of range1) {
   console.log(num);
+}
+
+// Problem
+let range = {
+  from: 1,
+  to: 5,
+
+  [Symbol.iterator]() {
+    this.current = this.from;
+    return this;
+  },
+
+  next() {
+    if (this.current <= this.to) {
+      return { done: false, value: this.current++ };
+    } else {
+      return { done: true };
+    }
+  },
+};
+
+for (let num of range) {
+  alert(num); // 1, then 2, 3, 4, 5
 }
