57a438af8b069b2f0a00b259c9dd5bc85df36da1
jonathan <jonathan@growth2success.io>
2025-01-17 11:58:03 +0530

    Global Objects complete

---
+++ JavaScript/05_Advance_with_Func/Global_Obj.js
@@ -0,0 +1,18 @@
+/// Global Object
+//      - inside Browser, it is "window"
+//      - in Node, it is "global"
+//      - all properties of Global Obj is accessed directly
+//      - in Global Object, properties and variables are declared with "var" only.
+
+/// For Browser
+var gVar = 5;
+alert(window.gVar); // 5
+
+let gLet = 5;
+alert(window.gLet); // undefined
+
+window.currentUser = {
+  name: "John",
+};
+alert(currentUser.name); // John
+alert(window.currentUser.name); // John
