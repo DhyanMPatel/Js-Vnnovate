5f5ff44808deb7d3c1680be3bad0f991baf610c4
jonathan <jonathan@growth2success.io>
2025-01-17 10:20:52 +0530

    pulled all changes

--- Js-Vnnovate/JavaScript/04_Data_Types/Numbers.js
+++ Js-Vnnovate/JavaScript/04_Data_Types/Numbers.js
@@ -1,6 +1,6 @@
 /// Numbers
 //      - limit -2^53 - 1 to 2^53 - 1 of Number
-//      -
+//      - num.toFixed() return String value
 
 let billion = 1000000000;
 let bill_ion = 1_000_000_000;
@@ -16,20 +16,35 @@ console.log(bill_ion === billion);
 let mcs = 1e-6; // Return - 0.000001, five zeroes to the left from 1
 
 /*
+/// Hexadecimal
+console.log(0xff); // Return - 255
+console.log(0xFF); // Return - 255
+/// Octal
+console.log(0o377); // Return - 255
+/// Binary
+console.log(0b11_111_111); // Return - 255
+*/
+
+
 /// .toString()
-console.log(bill.toString());
+console.log(bill.toString(10)); // Default
 console.log(bill.toString(16)); // Return - 3b9aca00
 
 console.log((123).toString()); // Return - 123, also 123..toString() possible
-*/
+
 
 /*
 /// Rounding
-//      - Math.floor(), Math.ciel(), Math.round(), Math.tranc()
+//      - floor(give lower value), 
+//      - ceil(give higher value), 
+//      - round(make round off), 
+//      - trunc(remove averything after decimal point only) (not supported by internet Exporar)
 //      - num.toFixed() internally use Math.round() means return will round() logic.
 
 let num = 1.23456;
-alert(Math.round(num * 100) / 100); // 1.23456 -> 123.456 -> 123 -> 1.23
+console.log(Math.round(num * 100) / 100); // 1.23456 -> 123.456 -> 123 -> 1.23
+console.log( +num.toFixed(2) ); // 1.23, in Number 
+console.log(0.1.toFixed(20)); // Return - 0.10000000000000000555
 */
 
 /*
@@ -43,15 +58,21 @@ alert(9999999999999999); // shows 10000000000000000
 
 /*
 /// isFinite & isNaN
+//  - give answer in Boolean
 //  - isFinite & isNaN methods convert their argument into number.
 //  - Number.isFinite() and Number.inNaN() both are more Strict compare to normal isFinite() and isNaN(). they will not autoconvert into numbers.
 
-alert(isNaN(NaN)); // true
-alert(isNaN("str")); // true
+console.log(isNaN(NaN));
+console.log(isNaN(123));
+console.log(isNaN("123.4"));
+console.log(isNaN("str")); // Return - true
+console.log(isNaN(Infinity)); // Return - false
 
-alert(isFinite("15")); // true
-alert(isFinite("str")); // false, because a special value: NaN
-alert(isFinite(Infinity)); // false, because a special value: Infinity
+console.log(isFinite(NaN)); // Return - false
+console.log(isFinite(123));
+console.log(isFinite("123.4"));
+console.log(isFinite("str")); // Return - false, because special value - NaN
+console.log(isFinite(Infinity)); // Return - false, because special value - Infinity
 
 alert( Number.isNaN("str") ); // false, because "str" belongs to the string type, not the number type
 alert( Number.isFinite("123") ); // false, because "123" belongs to the string type, not the number type
@@ -59,15 +80,17 @@ alert( Number.isFinite("123") ); // false, because "123" belongs to the string t
 
 /*
 /// parseInt & parseFloat
+//      - use for soft conversion
+//      - convert string to int, if string is not int, it will return NaN
 
-alert(parseInt("100px")); // 100
-alert(parseFloat("12.5em")); // 12.5
+console.log(parseInt("100px")); // 100
+console.log(parseFloat("12.5em")); // 12.5
 
-alert(parseInt("12.3")); // 12, only the integer part is returned
-alert(parseFloat("12.3.4")); // 12.3, the second point stops the reading
+console.log(parseInt("12.3")); // 12, only the integer part is returned
+console.log(parseFloat("12.3.4")); // 12.3, the second point stops the reading
 
-alert(parseInt("0xff", 16)); // 255
-alert(parseInt("ff", 16)); // 255, without 0x also works
+console.log(parseInt("0xff", 16)); // 255
+console.log(parseInt("ff", 16)); // 255, without 0x also works
 
 alert(parseInt("2n9c", 36)); // 123456
 */
@@ -78,7 +101,20 @@ alert(parseInt("2n9c", 36)); // 123456
 //  - Math.min(a,b,c,...)
 //  - Math.pow(n, power)
 
+// console.log(Math.random()); // [0,1)
+/*
+console.log(Math.max(3, 10, -10, -3, 20)); // Return - 20
+console.log(Math.min(3, 10, -10, -3, 20)); // Return - -10
+*/
+
+// console.log(Math.pow(2,3)); // Return - 8
+
 /// Experiment
 max = 0;
 min = -10;
-console.log(Math.floor(Math.random() * (max - min + 1) + min));
+// console.log(Math.floor(Math.random() * (max - min + 1) + min));
+
+// console.log(0.1 + 0.2 == 0.3); // false
+
+// console.log(6.35.toFixed(1)); // Return - 6.3 , there is Precision loss can cause both increase and decrease
+// console.log(Math.round(6.35 * 10) / 10); // Return - 6.4
