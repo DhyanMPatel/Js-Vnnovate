f4f7bc27f38043c17387e526b051142c7066f794
jonathan <jonathan@growth2success.io>
2025-01-20 12:28:45 +0530

    Arrow Function Revisited Different Behaviour complete

--- JavaScript/01_JS_Fundamentals/Functions/ArrowFunction.js
+++ JavaScript/01_JS_Fundamentals/Functions/ArrowFunction.js
@@ -1,8 +1,8 @@
 /// Arrow Function
+//      - Arrow Function has not it's "this", take "this" from outside means from sourding laxical scope
 
 let sum = (a, b) => a + b;
-console.log(sum(2,3));
+console.log(sum(2, 3));
 
-let sayHi = ()=> console.log("Hello")
+let sayHi = () => console.log("Hello");
 sayHi();
-
---
+++ JavaScript/05_Advance_with_Func/Arrow_Function.js
@@ -0,0 +1,53 @@
+/// Arrow Function with This
+//      - Arrow Function has not it's "this", take "this" from outside
+//      - Do not have this
+//      - Do not have arguments
+//      - Can’t be called with new
+//      - They also don’t have super
+
+/*
+let group = {
+  sayHii: "Hii",
+  students: ["Dhyan", "Raj", "Rahul", "Smit"],
+  showList() {
+    this.students.forEach((std) => console.log(`${this.sayHii}, ${std}`));
+  },
+  showListReg() {
+    this.students.forEach(
+      function (std) {
+        console.log(`${this.sayHii}, ${std} Reguar.`);
+      }.bind(this) // required to bind with regular function, that pass reference of group object
+    );
+  },
+};
+
+group.showList();
+group.showListReg(); // "this" return "undefined", "this.sayHii" refer provided arr "this.students"
+*/
+
+// do not have "arguments"
+
+function sayHii(who) {
+  console.log(`Hello, ${who}`);
+}
+
+//      - Ragular Function
+function deferRag(f, ms) {
+  return function (...args) {
+    let ctx = this;
+    setTimeout(function () {
+      return f.apply(ctx, args);
+    }, ms);
+  };
+}
+let sayHiiRag = deferRag(sayHii, 1000);
+sayHiiRag("Vnn");
+
+//      - Arrow Function
+function deferArrow(f, ms) {
+  return function () {
+    setTimeout(() => f.apply(this, arguments), ms);
+  };
+}
+let sayHiiArrow = deferArrow(sayHii, 2000);
+sayHiiArrow("Vnnovate");
