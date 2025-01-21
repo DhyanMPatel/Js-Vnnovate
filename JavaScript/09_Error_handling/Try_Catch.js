090857b3043628446e5e5396935972d30c6d8d77
Dhyan M Patel <dhyan.m.patel2021@gmail.com>
2025-01-21 23:40:27 +0530

    Try Catch without Global catch

--- JavaScript/09_Error_handling/Try_Catch.js
+++ JavaScript/09_Error_handling/Try_Catch.js
@@ -2,6 +2,14 @@
 //      - try...catch only works for runtime errors
 //      - try...catch works synchronously
 
+//      - Error has 3 main Properties
+//        1. err.name - Error name
+//        2. err.message - Texual message
+//        3. err.stack - Current call Stack
+//      - throw - generate an error
+//      - Error standerd type - Error, SyntaxError, ReferenceError, TypeError
+
+/*
 try {
   console.log("Start of try runs"); // (1) <--
 
@@ -11,7 +19,9 @@ try {
 } catch (err) {
   console.log(`Error has occurred!`); // (3) <--
 }
+*/
 
+/*
 // because the function itself is executed later, when the engine has already left the try...catch construct.
 try {
   setTimeout(function () {
@@ -19,4 +29,76 @@ try {
   }, 1000);
 } catch (err) {
   console.log("won't work");
+}
+*/
+
+/*
+// Solution - write try and catch inside setTimeout()
+setTimeout(function(){
+  try{
+    noSuchVariable;
+  } catch(err) {
+    console.log(`Error Name: ${err.name}`)
+    console.log(`Error message: ${err.message}`)
+    console.log(`Error Stack: ${err.stack}`)
+  }
+},1000);
+*/
+
+/*
+/// Modern JS
+try{
+  lalala;
+} catch {
+  console.log("Error has occurred!");
+}
+*/
+
+/*
+// throw Error 
+let json = '{ "age": 30 }'; // incomplete data
+try {
+
+  let user = JSON.parse(json); // <-- no errors
+  console.log( user.name ); // no name!, which is error for us but Object will not define as an error
+
+  if(!user.name){
+    throw new SyntaxError("Incomplete data: no name! ")
+  }
+
+} catch(err) {
+  console.log( "doesn't execute", err.name, err.message  );
+}
+*/
+
+/*
+// Rethrow - if we do't know how to deal with it.
+function readData(){
+  let json = '{ "age": 30 }'; // incomplete data
+  try{
+    let user = JSON.parse(json); // <-- no errors
+    lalala();
+  } catch(err){
+    console.log(`Error is Generate`)
+    if(!(err instanceof SyntaxError)){
+      throw err; // Rethrow
+    }
+  }
+}
+try{
+  readData();
+} catch(err){
+  console.log(`External catch: ${err}`);
+  
+}
+*/
+
+try {
+  let json = '{ "age": 30 }';
+  let user = JSON.parse(json); // <-- no errors
+  throw new ReferenceError("user is not define")
+} catch(err){
+  console.log(`${err.name}, ${err.message}`)
+} finally{
+  console.log(`Try...catch...finally complete`);
 }
