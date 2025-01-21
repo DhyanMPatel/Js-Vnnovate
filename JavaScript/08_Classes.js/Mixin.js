9e44f9a47d36a033a886696a1bd33a71b635e804
jonathan <jonathan@growth2success.io>
2025-01-21 19:26:00 +0530

    Mixin

---
+++ JavaScript/08_Classes.js/Mixin.js
@@ -0,0 +1,25 @@
+/// Mixin
+//      - make an object with useful methods, so that we can easily merge them into a prototype of any class.
+
+// mixin
+let sayHiMixin = {
+  sayHi() {
+    alert(`Hello ${this.name}`);
+  },
+  sayBye() {
+    alert(`Bye ${this.name}`);
+  },
+};
+
+// usage:
+class User {
+  constructor(name) {
+    this.name = name;
+  }
+}
+
+// copy the methods
+Object.assign(User.prototype, sayHiMixin);
+
+// now User can say hi
+new User("Dude").sayHi(); // Hello Dude!
