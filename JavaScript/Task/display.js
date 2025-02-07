505362debb48c3d8c6cdf8787042ff68b7516246
Dhyan M Patel <dhyan.m.patel2021@gmail.com>
2025-02-07 08:59:37 +0530

    perform Edit task

---
+++ JavaScript/Task/display.js
@@ -0,0 +1,66 @@
+function toIndex() {
+    window.location.href = "index.html";
+    localStorage.removeItem("Operation");
+    localStorage.setItem("Operation", "add")
+
+}
+
+function displayUsers() {
+    let users = JSON.parse(localStorage.getItem("UserData")) || [];
+    let tbody = document.querySelector("tbody");
+    tbody.innerHTML = "";
+
+
+    users.map((user, index) => {
+        let row = document.createElement("tr");
+        if(users.length == 0){
+            row.innerHTML = `
+                <td colspan ="11">There is No Data</td>
+            `
+        } else {
+        row.innerHTML = `
+        <td>${index + 1}</td>
+        <td>${user.firstName}</td>
+        <td>${user.lastName}</td>
+        <td>${user.gender}</td>
+        <td>${user.hobbies.join(", ") || "No Interest"}</td>
+        <td>${user.country}</td>
+        <td>${user.state}</td>
+        <td>${user.city}</td>
+        <td>${user.birthDate}</td>
+        <td>${user.birthTime}</td>
+        <td>
+            <button onclick="editUser(${index})" class="button"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
+                    <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325"/>
+                  </svg></button>
+            <button onclick="deleteUser(${index})" class="button"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
+                    <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5"/>
+                  </svg></button>
+        </td>
+        `
+        }
+
+        
+        tbody.appendChild(row);
+    })
+}
+
+// Function to delete user data
+function deleteUser(index) {
+    let users = JSON.parse(localStorage.getItem("UserData")) || [];
+    users.splice(index, 1); // Remove selected user
+    localStorage.setItem("UserData", JSON.stringify(users)); // Update localStorage
+    displayUsers(); // Refresh table
+}
+
+// Function to edit user data (redirect to the form page with data)
+function editUser(index) {
+    localStorage.setItem("editIndex", index); // Store index for editing
+    localStorage.removeItem('Operation')
+    localStorage.setItem('Operation',"edit")
+    window.location.href = "index.html"; // Redirect to form page
+}
+
+window.onload = function(){
+    displayUsers();
+}
--- JavaScript/Task/script.js
+++ JavaScript/Task/script.js
@@ -1,49 +1,153 @@
-let displayDiv = document.getElementById("displayDiv");
-let counter = 0;
 
-document.onsubmit = function handleForm(e) {
-  let hobies = document.querySelectorAll('input[class="hobies"]:checked');
-  let selectedCountry = document.getElementById("country");
-  let selectedState = document.getElementById("state");
-  let selectedCity = document.getElementById("city");
+function todisplay() {
+  window.location.href = "display.html";
+  document.forms['form'].reset()
+}
+
+let create = document.getElementById("create")
+create.onclick = function (e) {
+  e.preventDefault();
   let birthTime = document.getElementById("birthTime").value;
+  let hobbies = document.querySelectorAll('input[name="hobbies"]:checked');
   let amPm;
+  let arrTime = birthTime.split(":");
+  amPm = arrTime[0] >= 12 ? "PM" : "AM";
+  if (arrTime[0] > 12) {
+    if (arrTime[0] != 12) {
+      arrTime[0] = String(arrTime[0] - 12);
+    }
+  }
+  birthTime = `${arrTime.join(":")} ${amPm}`;
 
   let obj = {
     firstName: document.getElementById("firstName").value,
     lastName: document.getElementById("lastName").value,
     gender: document.querySelector('input[name="gender"]:checked').value,
-    hobies: [],
-    address: {
-      country: selectedCountry.options[selectedCountry.selectedIndex].text,
-      state: selectedState.options[selectedState.selectedIndex].text,
-      city: selectedCity.options[selectedCity.selectedIndex].text,
-    },
+    hobbies: [],
+    country: document.getElementById("country").value,
+    state: document.getElementById("state").value,
+    city: document.getElementById("city").value,
     birthDate: document.getElementById("birthDate").value,
     birthTime: birthTime,
-  };
-
-  for (let hobie of hobies) {
-    obj.hobies.push(hobie.value);
   }
 
-  let arrTime = birthTime.split(":");
+  for (let hobbie of hobbies) {
+    obj.hobbies.push(hobbie.value);
+  }
 
+  let users = JSON.parse(localStorage.getItem("UserData")) || [];
+  users.push(obj)
+
+
+  localStorage.setItem('UserData', JSON.stringify(users));
+  document.forms["form"].reset();
+  localStorage.removeItem("Operation")
+}
+let operation = localStorage.getItem('Operation')
+if (operation == "edit") {
+  document.getElementById("create").style.display = "none"
+  document.getElementById("update").style.display = "block"
+} else {
+  document.getElementById('create').style.display = "block"
+  document.getElementById('update').style.display = "none"
+}
+
+let cancel = document.getElementById('cancel');
+cancel.onclick = function () {
+  document.forms["form"].reset()
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  let editIndex = localStorage.getItem("editIndex");
+  if (editIndex !== null) {
+    let users = JSON.parse(localStorage.getItem("UserData")) || [];
+    let user = users[editIndex];
+
+    document.getElementById("firstName").value = user.firstName;
+    document.getElementById("lastName").value = user.lastName;
+    document.querySelector(`input[name="gender"][value="${user.gender}"]`).checked = true;
+
+    user.hobbies.forEach(hobby => {
+      let hobbyCheckbox = document.querySelector(`input[name="hobbies"][value="${hobby}"]`);
+      if (hobbyCheckbox) hobbyCheckbox.checked = true;
+    });
+
+    document.getElementById("country").value = user.country;
+    document.getElementById("state").value = user.state;
+    document.getElementById("city").value = user.city;
+    document.getElementById("birthDate").value = user.birthDate;
+    document.getElementById("birthTime").value = convertIn24(user.birthTime);
+
+    console.log(user);
+  }
+});
+
+function convertIn24(birthTime) {
+  let [time, period] = birthTime.split(" ")
+  let [hour, minute] = time.split(':')
+  let intHour = parseInt(hour);
+  intHour = parseInt(hour)
+  if (minute < 10 && minute > 0) {
+    minute = "0" + minute
+  }
+
+  if (period == "PM") {
+    if (intHour != 12) {
+      intHour = intHour + 12
+    }
+  }
+
+  if (intHour < 10 && intHour > 0) {
+    return `0${intHour}:${minute}`
+  }
+
+  return `${intHour}:${minute}`
+
+}
+
+let update = document.getElementById('update')
+update.addEventListener('click', function (e) {
+  let users = JSON.parse(localStorage.getItem("UserData")) || [];
+  let editIndex = localStorage.getItem("editIndex")
+  let user = users[editIndex]
+
+  e.preventDefault();
+  let birthTime = document.getElementById("birthTime").value;
+  let hobbies = document.querySelectorAll('input[name="hobbies"]:checked');
+  let amPm;
+  let arrTime = birthTime.split(":");
   amPm = arrTime[0] >= 12 ? "PM" : "AM";
   if (arrTime[0] > 12) {
-    arrTime[0] = String(arrTime[0] - 12);
+    if (arrTime[0] != 12) {
+      arrTime[0] = String(arrTime[0] - 12);
+    }
+  }
+  birthTime = `${arrTime.join(":")} ${amPm}`;
+
+  let obj = {
+    firstName: document.getElementById("firstName").value,
+    lastName: document.getElementById("lastName").value,
+    gender: document.querySelector('input[name="gender"]:checked').value,
+    hobbies: [],
+    country: document.getElementById("country").value,
+    state: document.getElementById("state").value,
+    city: document.getElementById("city").value,
+    birthDate: document.getElementById("birthDate").value,
+    birthTime: birthTime,
   }
 
-  obj.birthTime = `${arrTime.join(":")} ${amPm}`;
+  for (let hobbie of hobbies) {
+    obj.hobbies.push(hobbie.value);
+  }
 
-  let id = "UserData" + counter;
-  counter++;
-  localStorage.setItem(id, JSON.stringify(obj));
+  users[editIndex] = obj;
+  localStorage.removeItem('UserData')
+  localStorage.setItem("UserData", JSON.stringify(users));
 
-  document.forms["form"].reset();
-};
+  localStorage.removeItem("editIndex"); // Clear editIndex after loading data
+  localStorage.removeItem("Operation")
 
-let cancel = document.getElementById("cancel");
-cancel.onclick = function () {
-  document.forms["form"].reset();
-};
+  document.forms['form'].reset()
+  document.getElementById("update").style.display = "none";
+  document.getElementById('create').style.display = "block"
+})
