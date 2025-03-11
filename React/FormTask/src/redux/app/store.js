ef30c8c5561e3c341abf4ad483c75efc7a67517e
DhyanMPatel <dhyan.m.patel2021@gmail.com>
2025-03-11 18:06:43 +0530

    Change .jsx file to .js file in Redux

--- React/FormTask/src/redux/app/store.js
+++ React/FormTask/src/redux/app/store.js
@@ -1,8 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-import usersReducer from "./slice/userSlice";
-import showBtnReducer from "./slice/showBtnSlice";
-import editUserReducer from "./slice/editUserSlice";
-import initialValuesReducer from "./slice/initialValuesSlice";
+import usersReducer from "../slice/userSlice";
+import showBtnReducer from "../slice/showBtnSlice";
+import editUserReducer from "../slice/editUserSlice";
+import initialValuesReducer from "../slice/initialValuesSlice";
 
 const store = configureStore({
   reducer: {
