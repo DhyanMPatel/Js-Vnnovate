5c43a117a5b94cf9a0e8a4d4582f9c0a9220223a
Dhyan M Patel <dhyan.m.patel2021@gmail.com>
2025-06-27 10:08:41 +0530

    27 June 2025 Commit for last day work

--- JavaScript/01_JS_Fundamentals/Functions/Anonymous.js
+++ JavaScript/01_JS_Fundamentals/Functions/Anonymous.js
@@ -1,3 +1,10 @@
 setTimeout(function () {
-    console.log("Anonymous function executed!");
+  console.log("Anonymous function executed!");
 }, 1000);
+
+/*
+/// Gives error of statement required "function name"
+function () { 
+    console.log("This not possible"); 
+}
+*/
