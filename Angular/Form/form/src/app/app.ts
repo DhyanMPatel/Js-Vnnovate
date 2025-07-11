4ee779791db6814a0df4d576bc8abef9b89be909
Dhyan M Patel <dhyan.m.patel2021@gmail.com>
2025-07-11 18:10:32 +0530

    11 July 2025 6:09 PM Console Form Data in browser

--- Angular/Form/form/src/app/app.ts
+++ Angular/Form/form/src/app/app.ts
@@ -1,12 +1,59 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
-  protected title = 'form';
+  protected title = 'Form';
+
+  formData = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    contact: '',
+    gender: '',
+    subjects: {
+      maths: false,
+      science: false,
+      physics: false,
+      chemistry: false,
+      dsa: false,
+    },
+    location: {
+      city: '',
+    },
+    url: '',
+    about: '',
+  };
+
+  handleSubmit = () => {
+    console.log(this.formData);
+  }
+
+  resetForm = () => {
+    this.formData = {
+      firstName: '',
+      lastName: '',
+      email: '',
+      contact: '',
+      gender: '',
+      subjects: {
+        maths: false,
+        science: false,
+        physics: false,
+        chemistry: false,
+        dsa: false,
+      },
+      location: {
+        city: '',
+      },
+      url: '',
+      about: '',
+    };
+  };
 }
