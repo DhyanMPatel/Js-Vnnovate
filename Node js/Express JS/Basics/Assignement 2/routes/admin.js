5e766e99a9991a2e79e9fdc5c8e1de21c6b496bb
Dhyan M Patel <dhyan.m.patel2021@gmail.com>
2025-08-05 07:11:03 +0530

    5 August 2025 7:10 AM Commit to store body data in ProductData array

--- Node js/Express JS/Basics/Assignement 2/routes/admin.js
+++ Node js/Express JS/Basics/Assignement 2/routes/admin.js
@@ -4,18 +4,23 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { rootDir } = require('../utils/path');
 
-const router = express.Router();
-router.use(bodyParser.urlencoded({extended: false})); // This will parse only key-value pairs
+const ProductData = []; // Array to store product data
 
-router.get('/add-product', (req,res,next) => {
+const router = express.Router();
+router.use(bodyParser.urlencoded({ extended: false })); // This will parse only key-value pairs
+
+router.get('/add-product', (req, res, next) => {
     res.sendFile(path.join(__dirname, '..', 'views', 'add-product.html'));
     //      OR
     // res.sendFile(path.join(rootDir, 'views', 'add-product.html')); // More robust way to get the path
 });
 
-router.post('/product', (req,res,next) => {
+router.post('/product', (req, res, next) => {
     res.send('<h1>Product Added</h1>');
     console.log(req.body);  // Without bodyParser it will return undefined. because node by default does not parse the body.
+    ProductData.push({ title: req.body.title });
+    console.log(ProductData);
 })
 
-module.exports = router;
+exports.router = router;
+exports.productData = ProductData; // Exporting ProductData to use in other files
--- Node js/Express JS/Basics/Assignement 2/routes/shop.js
+++ Node js/Express JS/Basics/Assignement 2/routes/shop.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const { rootDir } = require('../utils/path');
+const productData = require('./admin').productData;
 
 const router = express.Router();
 
